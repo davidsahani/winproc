a93ea91a9f34f9088f3773fe624a1bd988c44273
David <97206601+davidsahani@users.noreply.github.com>
2026-03-17 21:53:38 +0530

    feat: introduce defer macro for scope_exit cleanups

---
+++ src/utils/Defer.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "ScopeExit.hpp"
+
+namespace scope {
+
+	struct ScopeExitHelper {
+		template <class F>
+		constexpr auto operator+(F &&f) const noexcept(
+			std::is_nothrow_constructible_v<ScopeExit<std::decay_t<F>>, F &&>
+		) -> ScopeExit<std::decay_t<F>> {
+			using Decayed = std::decay_t<F>;
+			return ScopeExit<Decayed>(Decayed(std::forward<F>(f)));
+		}
+	};
+
+	inline constexpr ScopeExitHelper scope_exit_helper{};
+
+} // namespace scope
+
+#define DEFER \
+[[maybe_unused]] auto UNIQUE_NAME(_defer_) = scope::scope_exit_helper + [&]()
